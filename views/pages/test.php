33dd00761cd5fd3f70574d831724669fb252364e
Rahim12345 <rahim.suleymanov94@gmail.com>
2023-07-06 21:13:55 +0400

    06.07 dersi

--- views/pages/test.php
+++ views/pages/test.php
@@ -8,15 +8,45 @@
         <p style="padding: 5px; text-align: justify">
 
             <?php
-                $arr = [1,2,3,5,7,10];
-
-                foreach ($arr as $rahim)
+            $eded = '';
+            $output = '';
+            if( isset($_POST['eded']) )
+            {
+                $eded = $_POST['eded'];
+                if (is_numeric($eded))
                 {
-                    echo $rahim.'<br>';
+                    if ( isset($_POST['tek_cut']) )
+                    {
+                        if ( $eded%2 == 0 )
+                        {
+                            $output = $eded." cutdur";
+                        }
+                        else
+                        {
+                            $output = $eded." tekdir";
+                        }
+                    }
+                    elseif ( isset($_POST['kv']) )
+                    {
+                        $output = $eded * $eded;
+                    }
+
                 }
+                else
+                {
+                    $output = "<b>$eded</b> eded deyildir";
+                }
+            }
+
             ?>
 
+        <form action="" method="post">
+            <input type="text" name="eded" value="<?= $eded ?>">
+            <input type="submit" name="tek_cut" value="Tek/Cut">
+            <input type="submit" name="kv" value="KV">
+        </form>
 
+        <?= $output ?>
         </p>
     </div>
 </main>
